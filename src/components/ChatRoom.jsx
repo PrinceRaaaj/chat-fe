f1845f4d543ea38b4531c903ae587b87d2a55320
Prince Raj <princefebraj08@gmail.com>
2022-01-04 20:58:55 +0530

    as

--- src/components/ChatRoom.jsx
+++ src/components/ChatRoom.jsx
@@ -50,6 +50,11 @@ const ChatRoom = () => {
             socket.emit("newMessage", {newMessage, room: data.room})
         }
     }
+    const onSubmit2 = () => {
+        fetch('https://chat-be-socket-io-prince.herokuapp.com/')
+        .then(response => response.json())
+        .then(data => console.log(data));
+    }
 
     return (
         <div className="py-4 m-5 w-50 shadow bg-white text-dark border rounded container" >
@@ -96,7 +101,10 @@ const ChatRoom = () => {
                                 <path d="M15.854.146a.5.5 0 0 1 .11.54l-5.819 14.547a.75.75 0 0 1-1.329.124l-3.178-4.995L.643 7.184a.75.75 0 0 1 .124-1.33L15.314.037a.5.5 0 0 1 .54.11ZM6.636 10.07l2.761 4.338L14.13 2.576 6.636 10.07Zm6.787-8.201L1.591 6.602l4.339 2.76 7.494-7.493Z"></path>
                             </svg>
                         }
-                    </button>   
+                    </button> 
+                    <button type="button" className="btn btn-warning mx-2" onClick={onSubmit2}>
+                        
+                    </button>    
                 </div>
         </div>
     )
